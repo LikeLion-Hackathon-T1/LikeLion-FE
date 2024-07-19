52f5c60411edc5f7e68a3ec8b1c7a163bbe7cafe
yseokkkkkk <yunseok0304@gmail.com>
2024-07-19 21:58:41 +0900

    feat: 가게 정보 페이지 구현

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,7 +10,8 @@ const HomePage = () => {
             <Header title="홈" back={false} />
             <KakaoMap />
             <button onClick={() => navigate("/menuTest")}>구매 테스트</button>
-            <button onClick={() => navigate("/market")}>마켓</button>
+            <button onClick={() => navigate("/market")}>시장 정보</button>
+            <button onClick={() => navigate("/store")}>가게 정보</button>
         </>
     );
 };
