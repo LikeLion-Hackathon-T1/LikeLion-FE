34b1bbf63dfb0852955870ebf9bf361011eb7c73
yunseok <tjr6613@seoultech.ac.kr>
2024-07-25 15:45:55 +0900

    feat: axios 기능 제한

--- src/utils/syluvAxios.js
+++ src/utils/syluvAxios.js
@@ -26,51 +26,68 @@ const CreateSyluvAxios = (navigate) => {
 
     syluvAxios.interceptors.request.use(
         function (config) {
-            return Promise.resolve(config);
+            console.log("Request sent:", config);
+            return config;
         },
-        (error) => Promise.reject(error)
+        (error) => {
+            console.error("Request error:", error);
+            return Promise.reject(error);
+        }
     );
 
     syluvAxios.interceptors.response.use(
         (response) => {
+            console.log("Response received:", response);
             return response;
         },
         function (error) {
-            const originalRequest = error.config;
-            if (error.response && !originalRequest._retry) {
-                originalRequest._retry = true;
-                return syluvAxios
-                    .get("/users/reissue", {
-                        headers: {
-                            RefreshToken: getRefreshToken(),
-                        },
-                    })
-                    .then((res) => {
-                        if (res.status === 200) {
-                            console.log("토큰 재발급 성공");
-                            setAccessToken(res.data.payload.accessToken);
-                            setRefreshToken(res.data.payload.refreshToken);
-                            axios.defaults.headers.common[
-                                "AccessToken"
-                            ] = `${res.data.payload.accessToken}`;
-                            return axios(originalRequest);
-                        }
-                    })
-                    .catch((reissueError) => {
-                        console.log("토큰 재발급 중 에러가 발생했습니다.");
-                        removeAccessToken();
-                        removeRefreshToken();
-                        console.log(reissueError);
-                        navigate("/login", { replace: true });
-                        return Promise.reject(reissueError);
-                    });
-            } else {
-                console.log("오류 발생:", error);
-                removeAccessToken();
-                removeRefreshToken();
-                navigate("/login", { replace: true });
-                return Promise.reject(error);
-            }
+            console.error("Response error:", error);
+            // 로그인 페이지로 이동
+            removeAccessToken();
+            removeRefreshToken();
+            navigate("/login", { replace: true });
+            console.log(error.response.status);
+            // const originalRequest = error.config;
+            // if (
+            //     error.response &&
+            //     error.response.status === 400 &&
+            //     !originalRequest._retry
+            // ) {
+            //     originalRequest._retry = true;
+            //     return syluvAxios
+            //         .get("/users/reissue", {
+            //             headers: {
+            //                 RefreshToken: getRefreshToken(),
+            //             },
+            //         })
+            //         .then((res) => {
+            //             if (res.status === 200) {
+            //                 console.log("토큰 재발급 성공");
+            //                 setAccessToken(res.data.payload.accessToken);
+            //                 setRefreshToken(res.data.payload.refreshToken);
+            //                 axios.defaults.headers.common[
+            //                     "AccessToken"
+            //                 ] = `${res.data.payload.accessToken}`;
+            //                 originalRequest.headers[
+            //                     "AccessToken"
+            //                 ] = `${res.data.payload.accessToken}`;
+            //                 return axios(originalRequest);
+            //             }
+            //         })
+            //         .catch((reissueError) => {
+            //             console.error(
+            //                 "토큰 재발급 중 에러가 발생했습니다:",
+            //                 reissueError
+            //             );
+            //             removeAccessToken();
+            //             removeRefreshToken();
+            //             navigate("/login", { replace: true });
+            //             return Promise.reject(reissueError);
+            //         });
+            // } else {
+            //     console.error("오류 발생:", error);
+            //     return Promise.reject(error);
+            // }
         }
     );
 
