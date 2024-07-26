73985e3da2223bcbf0b3215e0f97dd98376bbf0f
yunseok <tjr6613@seoultech.ac.kr>
2024-07-27 00:02:01 +0900

    feat: 리뷰 페이지 완성

--- src/components/Store/StoreInfo.js
+++ src/components/Store/StoreInfo.js
@@ -19,198 +19,210 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const StoreInfo = ({
-  call = "010-3164-1145",
-  address = "서울시 마포구 연남동 255-23",
-  time = "05:30 ~ 19:30 (일요일 휴무)",
-  rating = 4.2, // rating 기본값 설정
+    call = "010-3164-1145",
+    address = "서울시 마포구 연남동 255-23",
+    time = "05:30 ~ 19:30 (일요일 휴무)",
+    rating = 4.2, // rating 기본값 설정
 }) => {
-  const navigate = useNavigate();
-  const images = [
-    headerImage1,
-    headerImage2,
-    headerImage3,
-    headerImage4,
-    headerImage5,
-    headerImage6,
-    headerImage7,
-  ];
-  const [currentSlide, setCurrentSlide] = useState(0);
+    const navigate = useNavigate();
+    const images = [
+        headerImage1,
+        headerImage2,
+        headerImage3,
+        headerImage4,
+        headerImage5,
+        headerImage6,
+        headerImage7,
+    ];
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    afterChange: (index) => setCurrentSlide(index),
-  };
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        afterChange: (index) => setCurrentSlide(index),
+    };
 
-  // 별과 빈 별의 갯수 계산
-  const fullStars = Math.floor(rating);
-  const emptyStars = 5 - fullStars;
+    // 별과 빈 별의 갯수 계산
+    const fullStars = Math.floor(rating);
+    const emptyStars = 5 - fullStars;
 
-  return (
-    <Container>
-      <ImageContainer>
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <HeaderImage key={index} src={image} alt={`Store-${index}`} />
-          ))}
-        </Slider>
-        <BackButton onClick={() => navigate(-1)}>
-          <BackIcon />
-        </BackButton>
-        <ImageCounter>
-          {currentSlide + 1} / {images.length}
-        </ImageCounter>
-      </ImageContainer>
-      <InfoContainer>
-        <SubTitle>분식</SubTitle>
-        <Title>원조 누드치즈김밥</Title>
-        <Rating>
-          <Stars>
-            {Array(fullStars)
-              .fill()
-              .map((_, i) => (
-                <img key={`full-${i}`} src={starIcon} alt="star" />
-              ))}
-            {Array(emptyStars)
-              .fill()
-              .map((_, i) => (
-                <img key={`empty-${i}`} src={emptyStarIcon} alt="empty star" />
-              ))}
-          </Stars>
-          <RatingValue>{rating.toFixed(1)}</RatingValue>
-        </Rating>
-        <Info>
-          <CallIcon />
-          <InfoTitle>전화번호</InfoTitle>
-          <InfoDetail>{call}</InfoDetail>
-        </Info>
-        <Info>
-          <AddressIcon />
-          <InfoTitle>주소</InfoTitle>
-          <InfoDetail>{address}</InfoDetail>
-        </Info>
-        <Info>
-          <TimeIcon />
-          <InfoTitle>영업시간</InfoTitle>
-          <InfoDetail>{time}</InfoDetail>
-        </Info>
-      </InfoContainer>
-    </Container>
-  );
+    return (
+        <Container>
+            <ImageContainer>
+                <Slider {...settings}>
+                    {images.map((image, index) => (
+                        <HeaderImage
+                            key={index}
+                            src={image}
+                            alt={`Store-${index}`}
+                        />
+                    ))}
+                </Slider>
+                <BackButton onClick={() => navigate(-1)}>
+                    <BackIcon />
+                </BackButton>
+                <ImageCounter>
+                    {currentSlide + 1} / {images.length}
+                </ImageCounter>
+            </ImageContainer>
+            <InfoContainer>
+                <SubTitle>분식</SubTitle>
+                <Title>원조 누드치즈김밥</Title>
+                <Rating>
+                    <Stars>
+                        {Array(fullStars)
+                            .fill()
+                            .map((_, i) => (
+                                <img
+                                    key={`full-${i}`}
+                                    src={starIcon}
+                                    alt="star"
+                                />
+                            ))}
+                        {Array(emptyStars)
+                            .fill()
+                            .map((_, i) => (
+                                <img
+                                    key={`empty-${i}`}
+                                    src={emptyStarIcon}
+                                    alt="empty star"
+                                />
+                            ))}
+                    </Stars>
+                    <RatingValue>{rating.toFixed(1)}</RatingValue>
+                </Rating>
+                <Info>
+                    <CallIcon />
+                    <InfoTitle>전화번호</InfoTitle>
+                    <InfoDetail>{call}</InfoDetail>
+                </Info>
+                <Info>
+                    <AddressIcon />
+                    <InfoTitle>주소</InfoTitle>
+                    <InfoDetail>{address}</InfoDetail>
+                </Info>
+                <Info>
+                    <TimeIcon />
+                    <InfoTitle>영업시간</InfoTitle>
+                    <InfoDetail>{time}</InfoDetail>
+                </Info>
+            </InfoContainer>
+        </Container>
+    );
 };
 
 const Container = styled.div`
-  padding: 0 0px;
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  box-shadow: none;
-  margin-bottom: 20px;
+    padding: 0 0px;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    box-shadow: none;
+    margin-bottom: 20px;
 `;
 
 const ImageContainer = styled.div`
-  position: relative;
-  width: 100%;
-  padding: 0;
-  margin: 0;
+    position: relative;
+    width: 100%;
+    padding: 0;
+    margin: 0;
 `;
 
 const HeaderImage = styled.img`
-  width: 100%;
-  height: auto;
-  display: block;
-  margin: 0;
+    width: 100%;
+    height: auto;
+    display: block;
+    margin: 0;
 `;
 
 const BackButton = styled.div`
-  position: absolute;
-  top: 16px;
-  left: 16px;
-  cursor: pointer;
-  padding: 8px;
+    position: absolute;
+    top: 16px;
+    left: 16px;
+    cursor: pointer;
+    padding: 8px;
 `;
 
 const ImageCounter = styled.div`
-  position: absolute;
-  bottom: 16px;
-  right: 24px;
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-  padding: 4px 8px;
-  border-radius: 31px;
-  font-size: 12px;
-  width: 52px;
-  height: 26px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
+    position: absolute;
+    bottom: 16px;
+    right: 24px;
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 4px 8px;
+    border-radius: 31px;
+    font-size: 12px;
+    width: 52px;
+    height: 26px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: ${({ theme }) => theme.fontWeight.regular};
 `;
 
 const InfoContainer = styled.div`
-  margin: 20px 20px 0 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
+    margin: 20px 20px 0 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
 `;
 
 const Info = styled.div`
-  display: flex;
-  gap: 4px; /* 4px 간격 */
-  align-items: center;
+    display: flex;
+    gap: 4px; /* 4px 간격 */
+    align-items: center;
 `;
 
 const InfoTitle = styled.div`
-  font-size: 14px;
-  font-weight: ${({ theme }) => theme.fontWeight.medium};
-  color: ${({ theme }) => theme.color.gray800};
+    font-size: 14px;
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+    color: ${({ theme }) => theme.color.gray800};
 `;
 
 const InfoDetail = styled.div`
-  font-size: 14px;
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
-  color: ${({ theme }) => theme.color.gray500};
+    font-size: 14px;
+    font-weight: ${({ theme }) => theme.fontWeight.regular};
+    color: ${({ theme }) => theme.color.gray500};
 `;
 
 const SubTitle = styled.h2`
-  font-size: 14px;
-  color: ${({ theme }) => theme.color.gray600};
-  font-weight: ${({ theme }) => theme.fontWeight.medium};
+    font-size: 14px;
+    color: ${({ theme }) => theme.color.gray600};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
 `;
 
 const Title = styled.h1`
-  font-size: 20px;
-  margin-top: 8px;
-  margin-bottom: 12px;
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-  color: ${({ theme }) => theme.color.gray900};
+    font-size: 20px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    color: ${({ theme }) => theme.color.gray900};
 `;
 
 const Rating = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
 `;
 
 const Stars = styled.div`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 
-  & img {
-    width: 14px;
-    height: 14px;
-    margin-right: 1px;
-  }
+    & img {
+        width: 14px;
+        height: 14px;
+        margin-right: 1px;
+    }
 `;
 
 const RatingValue = styled.span`
-  color: ${({ theme }) => theme.color.gray900};
-  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
-  font-size: 14px;
-  margin-left: 4px;
+    color: ${({ theme }) => theme.color.gray900};
+    font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+    font-size: 14px;
+    margin-left: 4px;
 `;
 
 export default StoreInfo;
--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -3,48 +3,107 @@ import styled from "styled-components";
 import star from "assets/images/star-empty.png";
 import starFilled from "assets/images/star-fill.png";
 import add from "assets/images/add-button.png";
+import { useEffect, useState } from "react";
+import Button from "components/Common/Button";
 
 const ReviewPage = () => {
+    const [review, setReview] = useState("");
+    const [currentCount, setCurrentCount] = useState(0);
+    const [rating, setRating] = useState(0);
+    const [ratingText, setRatingText] = useState("");
+
+    useEffect(() => {
+        setCurrentCount(review.length);
+    }, [review]);
+
+    const handleStarClick = (index) => {
+        setRating(index + 1);
+    };
+
+    useEffect(() => {
+        const ratingTexts = [
+            "가게를 평가해주세요",
+            "다시 오고 싶지 않아요",
+            "생각보다 별로에요",
+            "괜찮네요",
+            "또 오고 싶어요",
+            "최고의 가게, 추천합니다!",
+        ];
+        setRatingText(ratingTexts[rating]);
+    }, [rating]);
+
     return (
-        <>
-            <Header title="후기 작성하기" cart={false} />
-            <Menu>
-                <img src="https://via.placeholder.com/375x375" alt="menu" />
-                <div className="store-info">
-                    <div>
-                        <span className="store-name">진아 수산</span>
-                        <span className="menu-name">연어 회</span>
-                    </div>
-                    <span>2024년 7월 17일 오후 08:12</span>
-                </div>
-            </Menu>
-            <Review>
-                <div className="rating-section">
-                    <span className="rating-title">
-                        이 가게를 추천하시겠어요?
-                    </span>
-                    <div className="rating-star">
-                        <div className="rating-image">
-                            <img src={starFilled} alt="star" />
-                            <img src={starFilled} alt="star" />
-                            <img src={starFilled} alt="star" />
-                            <img src={starFilled} alt="star" />
-                            <img src={starFilled} alt="star" />
+        <Wrapper>
+            <div>
+                <Header title="후기 작성하기" cart={false} />
+                <Menu>
+                    <img src="https://via.placeholder.com/375x375" alt="menu" />
+                    <div className="store-info">
+                        <div>
+                            <span className="store-name">진아 수산</span>
+                            <span className="menu-name">연어 회</span>
                         </div>
-                        <span>최고의 가게, 추천합니다</span>
+                        <span>2024년 7월 17일 오후 08:12</span>
                     </div>
-                </div>
-                <div className="review-section">
-                    <span>음식의 맛, 양 포장 상태 등</span>
-                </div>
-                <div className="photo-section">
-                    <span>사진을 추가해주세요</span>
-                    <img src={add} alt="review" />
-                </div>
-            </Review>
-        </>
+                </Menu>
+                <Review>
+                    <div className="rating-section">
+                        <span className="rating-title">
+                            이 가게를 추천하시겠어요?
+                        </span>
+                        <div className="rating-star">
+                            <div className="rating-image">
+                                {[...Array(5)].map((_, index) => (
+                                    <img
+                                        key={index}
+                                        src={index < rating ? starFilled : star}
+                                        alt="star"
+                                        onClick={() => handleStarClick(index)}
+                                    />
+                                ))}
+                            </div>
+                            <span>{ratingText}</span>
+                        </div>
+                    </div>
+                    <div className="review-section">
+                        <textarea
+                            className="review-input"
+                            type="text"
+                            placeholder="가게와 상품에 대한 솔직한 리뷰를 남겨주세요."
+                            value={review}
+                            onChange={(e) => setReview(e.target.value)}
+                        />
+                        <div className="review-count">
+                            <span className="current-count">
+                                {currentCount}
+                            </span>
+                            /300
+                        </div>
+                    </div>
+                    <div className="photo-section">
+                        <span>사진을 추가해주세요</span>
+                        <img src={add} alt="review" />
+                    </div>
+                </Review>
+            </div>
+            <BottomBar>
+                <Button text="등록하기" type="2" />
+            </BottomBar>
+        </Wrapper>
     );
 };
+
+const BottomBar = styled.div`
+    padding: 0 20px 25px 20px;
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    min-height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
 
 const Review = styled.div`
     display: flex;
@@ -71,9 +130,15 @@ const Review = styled.div`
         align-items: center;
         gap: 12px;
 
-        .rating-star {
+        .rating-image {
             display: flex;
             gap: 2px;
+
+            img {
+                &:hover {
+                    cursor: pointer;
+                }
+            }
         }
 
         span {
@@ -81,6 +146,68 @@ const Review = styled.div`
             font-weight: ${({ theme }) => theme.fontWeight.semiBold};
             color: ${({ theme }) => theme.color.primary};
             margin-bottom: 32px;
+        }
+    }
+
+    .review-section {
+        width: 100%;
+        display: flex;
+        position: relative;
+        .review-input {
+            width: 100%;
+            height: 184px;
+            border: 1px solid ${({ theme }) => theme.color.gray100};
+            border-radius: 12px;
+            padding: 16px;
+            resize: none;
+            outline: none;
+            font-size: 14px;
+            font-weight: ${({ theme }) => theme.fontWeight.regular};
+            color: ${({ theme }) => theme.color.gray900};
+            &::placeholder {
+                color: ${({ theme }) => theme.color.gray300};
+            }
+            margin-bottom: 20px;
+        }
+        .review-count {
+            position: absolute;
+            bottom: 40px;
+            right: 20px;
+            display: flex;
+            font-size: 14px;
+            font-weight: ${({ theme }) => theme.fontWeight.medium};
+            color: ${({ theme }) => theme.color.gray300};
+
+            .current-count {
+                color: ${({ theme }) => theme.color.primary};
+            }
+        }
+    }
+
+    .photo-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 154px;
+        background-color: ${({ theme }) => theme.color.gray50};
+        gap: 11px;
+        border-radius: 8px;
+        border: 1px solid ${({ theme }) => theme.color.gray100};
+
+        span {
+            font-size: 18px;
+            font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+            color: ${({ theme }) => theme.color.gray900};
+        }
+
+        img {
+            &:hover {
+                cursor: pointer;
+                transform: scale(1.08);
+                transition: transform 0.1s;
+            }
         }
     }
 `;
