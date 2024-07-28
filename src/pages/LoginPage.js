c246bdf6d8e4dc62e9ab818edeae5f4738ca2f4c
yunseok <tjr6613@seoultech.ac.kr>
2024-07-29 02:12:40 +0900

    feat: 로그인 페이지 수정

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import useIsLogin from "hooks/useIsLogin";
@@ -7,6 +7,7 @@ import { ReactComponent as SyluvLogo } from "assets/images/syluv.svg";
 import Splash from "components/Common/Splash";
 
 const LoginPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
     const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
     const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -17,12 +18,22 @@ const LoginPage = () => {
     const isLogin = useIsLogin();
 
     useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+
+        return () => clearTimeout(timer); // Cleanup the timeout if the component unmounts
+    }, []);
+
+    useEffect(() => {
         if (isLogin) {
             navigate("/", { replace: true });
         }
     }, [isLogin, navigate]);
 
-    return (
+    return isLoading ? (
+        <Splash />
+    ) : (
         <Container>
             <Header>
                 <Title>달콤한 시장 나들이,</Title>
@@ -76,6 +87,13 @@ const LoginButton = styled.button`
     border: none;
     background-color: transparent;
     cursor: pointer;
+
+    &:hover {
+        transform: scale(1.01);
+        transition: transform 0.2s;
+
+        filter: brightness(1.02);
+    }
 `;
 
 export default LoginPage;
