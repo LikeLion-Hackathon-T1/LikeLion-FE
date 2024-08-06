7918018fa364666e9f4b17ec22c97b4571a515a7
yunseok <tjr6613@seoultech.ac.kr>
2024-08-06 21:59:59 +0900

    feat: 현장 QA반영

--- src/components/Market/EditList.js
+++ src/components/Market/EditList.js
@@ -2,13 +2,10 @@ import styled from "styled-components";
 import EditVisitItem from "./EditVisitItem";
 
 const EditList = ({ visitList, handleSelect = () => {} }) => {
-    const filteredVisitList = visitList.filter(
-        (item) => item.status === "BEFORE"
-    );
     return (
         <Wrapper>
             <ListContainer>
-                {filteredVisitList.map((item, index) => (
+                {visitList.map((item, index) => (
                     <EditVisitItem
                         item={item}
                         key={index} // 추가된 부분: key prop
@@ -16,7 +13,6 @@ const EditList = ({ visitList, handleSelect = () => {} }) => {
                     />
                 ))}
             </ListContainer>
-            <Infomation>방문 전인 가게만 삭제할 수 있어요</Infomation>
         </Wrapper>
     );
 };
@@ -27,29 +23,6 @@ const Wrapper = styled.div`
     position: relative;
     height: 58dvh;
     padding-bottom: 90px;
-`;
-
-const Infomation = styled.div`
-    position: fixed;
-    width: 440px;
-    margin-bottom: 20px;
-    margin-left: 20px;
-    height: 43px;
-    border-radius: 4px;
-    background-color: ${({ theme }) => theme.color.gray400};
-    bottom: 0;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    font-size: 14px;
-    color: white;
-    font-weight: ${({ theme }) => theme.fontWeight.medium};
-
-    @media (max-width: 480px) {
-        width: calc(100vw - 40px);
-    }
 `;
 
 const ListContainer = styled.div`
